e2911ded9930b1dffb24ff27eb48ccd321e4a5a0
Dan Rusk <drusk@carrollcc.edu>
2015-09-08 19:41:35 -0400

    One possible solution

--- FileRandomizer/src/cis232/lab/Randomizer.java
+++ FileRandomizer/src/cis232/lab/Randomizer.java
@@ -2,7 +2,9 @@ package cis232.lab;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Randomizer {
@@ -12,16 +14,18 @@ public class Randomizer {
 		File originalFile = new File("original_list.txt");
 		Scanner input = new Scanner(originalFile);
 		ArrayList<String> lines = new ArrayList<>();
+		Random random = new Random();
+		PrintWriter output = new PrintWriter("randomized_list.txt");
 		
 		while(input.hasNextLine()){
 			lines.add(input.nextLine());
 		}
 
-		//For testing purposes, output all the lines
-		for(String line : lines){
-			System.out.println(line);
+		while(!lines.isEmpty()){
+			output.println(lines.remove(random.nextInt(lines.size())));
 		}
 		
+		output.close();
 		input.close();
 	}
 
